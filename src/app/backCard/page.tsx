654794e8af8bf5550486976713c68f82cc8d1baf
mazhar mazhar <mazharkamboh914@gmail.com>
2023-12-18 22:47:39 +0500

    I have created new MemoryCardGame in next.js using typescript ,tailwind css and I add toast message at time of completed game

---
+++ src/app/backCard/page.tsx
@@ -0,0 +1,11 @@
+const BackCard = () => {
+  return (
+    <>
+      <div className='absolute left-0 w-32 h-32 bg-red-600  flex justify-center items-center'>
+        {/* <img className='h-32  rounded-lg' src="/backcard.png" alt="Memory Card Back" /> */}
+      </div>
+    </>
+  );
+}
+
+export default BackCard;
